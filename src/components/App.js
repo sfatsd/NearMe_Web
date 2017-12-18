911420ac37494009d84b603f6d0f9c1ece6a0d70
Wei-Yuan Shih <Frank@Wei-Yuans-MacBook-Pro.local>
2017-12-17 20:13:19 -0800

    login complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 import { Main } from './Main';
+import { TOKEN_KEY } from '../constants';
 import '../styles/App.css';
 
 class App extends Component {
+  state = {
+      isLoggedIn: false,
+  }
+
+  handleLogin = (token) => {
+      localStorage.setItem( TOKEN_KEY , token);
+      this.setState({ isLoggedIn: true });
+  }
+
   render() {
     return (
       <div className="App">
-          <Header/>
-          <Main/>
+          <Header isLoggedIn = {this.state.isLoggedIn}/>
+          <Main isLoggedIn = {this.state.isLoggedIn} handleLogin={this.handleLogin}/>
       </div>
     );
   }
