012efb466c746feaaf991ab2da3a09ec49df905f
Wei-Yuan Shih <Frank@Wei-Yuans-MacBook-Pro.local>
2017-12-20 22:14:26 -0800

    get geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Button } from 'antd';
+import { GEO_OPTIONS } from "../constants";
+
 const TabPane = Tabs.TabPane;
 
-function callback(key) {
-    console.log(key);
-}
+const operations = <Button>Extra Action</Button>;
 
 
 
 export class Home extends React.Component {
+
+    getGeoLocation = () => {
+        if (navigator && navigator.geolocation) {
+            this.setState({ loadingGeoLocation: true, error: '' });
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS
+            );
+        } else {
+            this.setState({ error: 'Your browser does not support geolocation!'});
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position)
+    }
+
+    onFailedLoadGeoLocation = () => {
+        this.setState({ loadingGeoLocation: false, error: 'Failed to load geo location!' });
+    }
+
+    componentDidMount() {
+        this.getGeoLocation();
+    }
+
     render() {
         return (
-            <Tabs defaultActiveKey="1" onChange={callback}>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
                 <TabPane tab="Posts" key="1">Content of Tab Pane 1</TabPane>
                 <TabPane tab="Map" key="2">Content of Tab Pane 2</TabPane>
             </Tabs>
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const API_ROOT = 'https://around-75015.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true, // remove if it takes too long to load
+    maximumAge: 3600000,
+    timeout: 30000,
+};
