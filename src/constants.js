2fdfae34dfc9b1e9f5a2ee28e034324545b3a3bd
Wei-Yuan Shih <Frank@wei-yuans-mbp.lan>
2018-11-14 14:19:40 -0800

    end stage 3, create post boutton

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,9 @@
 export const API_ROOT = 'https://around-75015.appspot.com';
 export const TOKEN_KEY = 'TOKEN_KEY';
-export const POS_KEY = 'POS';
+export const POS_KEY = 'POS_KEY';
 export const GEO_OPTIONS = {
-    enableHighAccuracy: false, // remove if it takes too long to load
+    enableHighAccuracy: true,
     maximumAge: 3600000,
-    timeout: 30000,
+    timeout: 27000,
 };
+export const AUTH_PREFIX = 'Bearer';
